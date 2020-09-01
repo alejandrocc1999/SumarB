5d4c573ee2de3f561a19dea2c71c838303c3bdfa
Victor Alejandro Castro Chavez <a2017113557@estudiantes.upsa.edu.bo>
2020-09-01 11:25:32 -0400

    MultiplyController

--- Sumar_Restar_B/UnitTestMultiply/UnitTest1.cs
+++ Sumar_Restar_B/UnitTestMultiply/UnitTest1.cs
@@ -1,21 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SumarB.Controllers;
 
-namespace UnitTestSumar
+namespace UnitTestMultiply
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void AddTest1()
+        public void MultiplyTest1()
         {
             //Arrange
-            SumaController sumaController = new SumaController();
+            SumaController multiplyController = new SumaController();
             int a = 2;
-            int b = 3;
-            int expected = 5;
+            int b = 5;
+            int expected = 10;
             //Act
-            int resultado = sumaController.Add(a, b);
+            int resultado = multiplyController.Mul(a, b);
             //Assert
             Assert.AreEqual(expected, resultado);
         }
